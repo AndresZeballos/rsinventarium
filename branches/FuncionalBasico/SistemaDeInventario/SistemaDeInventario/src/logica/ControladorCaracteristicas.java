2f273ed5ca2a44dfa7059beb3d4a98340b14b845
andreszeballosjuanico@gmail.com <andreszeballosjuanico@gmail.com@ba2a8e1c-2f8a-2fba-bfcc-4384e9df37bb>
2012-09-21 11:12:27 +0000

    Esta version es funcional, pero sin la controladores de Software as a Service. Se planea desarrollar un chequeo previo al startup de la aplicación donde se verifica que exista una entrada en la base de datos correspodiente al mes actual y al mes siguiente. Con el pago mensual del mantenimiento se realizará la actualización de la BD para agregar la entrada correspondiente al siguiente mes. Esto se hará con un script (.sql) para optimizar la operativa. El usuario "usuario" (que se quitara del .ini de configuración, se dejará su password solamente) de la BD tendrá acceso solamente de lectura sobre esta tabla. La documentación detallada estará en el branche correspondiente.

---
+++ branches/FuncionalBasico/SistemaDeInventario/SistemaDeInventario/src/logica/ControladorCaracteristicas.java
@@ -0,0 +1,86 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package logica;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.List;
+import sistemadeinventario.ConectionH;
+
+/**
+ *
+ * @author Andres
+ */
+public class ControladorCaracteristicas {
+
+    private Hashtable<String, List> caracteristicas;
+    
+    public ControladorCaracteristicas() {
+        initCaracteristicas();
+    }
+    
+    public void initCaracteristicas() {
+        this.caracteristicas = new Hashtable<String, List>();
+        String[] tablas = {
+            // Caracteristicas propias de la descripción de los articulos
+            /*
+            "pinzas",
+            "telas",
+            "estaciones",
+            "tipopantalon",
+            "tipoestampado",
+            "cuellos",
+            "mangas",
+            "tiporemera",
+            "lisorayado",
+            "tipocampera",
+            "lisafantasia",
+            "tiposuela",
+            "tipotirador",
+            "tipocinturon",
+            "tipopack",
+            "vbotones",
+            "vbase",
+            "cortolargo",
+            */
+            //
+            "colores",
+            "talles",
+            "locales",
+            "marcas",
+            "categorias",
+            "descripciones",
+            "componentes"
+        };
+        ConectionH c = new ConectionH();
+        Statement stmt = c.getStatement();
+        ResultSet rs;
+        List<String> lista;
+        for (String tabla : tablas) {
+            try {
+                rs = stmt.executeQuery("SELECT * from " + tabla);
+                lista = new ArrayList<String>();
+                while (rs.next()) {
+                    lista.add(rs.getString(1));
+                }
+                this.caracteristicas.put(tabla, lista);
+            } catch (SQLException e) {
+            }
+        }
+    }
+
+    public List<String> getCaracteristica(String caracteristica) {
+        return this.caracteristicas.get(caracteristica);
+    }
+    
+    public boolean existeElementoCaracteristica(String elemento, String caracteristica){
+        List a = this.caracteristicas.get(caracteristica);
+        return a.contains(elemento);
+    }
+    
+}
