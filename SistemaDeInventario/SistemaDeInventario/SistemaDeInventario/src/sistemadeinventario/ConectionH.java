ceda509150bfda5d5dd631bfefa063afd325492f
andreszeballosjuanico@gmail.com <andreszeballosjuanico@gmail.com@ba2a8e1c-2f8a-2fba-bfcc-4384e9df37bb>
2012-11-06 19:37:29 +0000

    Se comenzó con la documentación del código y su optimización.

--- SistemaDeInventario/SistemaDeInventario/SistemaDeInventario/src/sistemadeinventario/ConectionH.java
+++ SistemaDeInventario/SistemaDeInventario/SistemaDeInventario/src/sistemadeinventario/ConectionH.java
@@ -5,6 +5,14 @@ import java.sql.*;
 import java.util.Properties;
 import javax.swing.JOptionPane;
 
+/*
+ * Esta clase levanta el archivo de configuración ubicado en:
+ * "C:\\Sistema de RossiSport\\params.ini"
+ * Y utiliza sus valores como parametro para levantar la conección con 
+ * la base de datos.
+ * Mediante el método getStatement se puede obtener una instancia de 
+ * Statement para realizar las consultas a la base de datos.
+ */
 public class ConectionH {
 
     private Statement stmt;
@@ -12,7 +20,6 @@ public class ConectionH {
     private String driver, jdbc, host, puerto, base, usuario, contraseña;
     private boolean ok;
 
-    
     public boolean getOk() {
         return this.ok;
     }
@@ -20,17 +27,20 @@ public class ConectionH {
         return this.stmt;
     }
 
-    /*
-     public Statement getStatement2() {
-     try {
-     stmt = con.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE,
-     ResultSet.CONCUR_READ_ONLY);
-     } catch (SQLException e) {
-     e.printStackTrace();
-     stmt = null;
-     }
-     return stmt;
-     }*/
+    public ConectionH() {
+        leer_ini();
+        try {
+            Class.forName(driver); // Class.forName("com.mysql.jdbc.Driver");
+            String url = jdbc + host + puerto + base; // "jdbc:mysql://localhost:3306/rossisport";
+            con = DriverManager.getConnection(url, usuario, contraseña); // DriverManager.getConnection(url, "root", "");
+            stmt = con.createStatement();
+            this.ok = true;
+        } catch (Exception e) {
+            this.ok = false;
+            JOptionPane.showConfirmDialog(null, "Ocurrió un problema al conectarse a la base de datos", "Error en la conección", JOptionPane.CLOSED_OPTION, JOptionPane.WARNING_MESSAGE);
+        }
+    }
+    
     private void leer_ini() {
         try {
             Properties p = new Properties();
@@ -40,38 +50,12 @@ public class ConectionH {
             host = p.getProperty("host");
             puerto = p.getProperty("puerto");
             base = p.getProperty("base");
+            // Si no está el parametro user, utiliza uno por defecto ("usuario")
             usuario = p.getProperty("user", "usuario");
             contraseña = p.getProperty("data");
         } catch (Exception e) {
             this.ok = false;
             JOptionPane.showConfirmDialog(null, "Ocurrió un problema", "Error al leer la configuración", JOptionPane.CLOSED_OPTION, JOptionPane.WARNING_MESSAGE);
-        }
-    }
-
-    public ConectionH() {
-        leer_ini();
-        try {
-            Class.forName(driver);
-            String url = jdbc + host + puerto + base;// "jdbc:mysql://localhost:3306/rossisport";
-            con = DriverManager.getConnection(url, usuario, contraseña);
-            stmt = con.createStatement();
-            this.ok = true;
-            /*
-            Class.forName("com.mysql.jdbc.Driver");
-            String url = "jdbc:mysql://localhost:3306/rossisport";
-            con = DriverManager.getConnection(url, "root", "");
-            stmt = con.createStatement();
-            */
-        } catch (Exception e) {
-            this.ok = false;
-            JOptionPane.showConfirmDialog(null, "Ocurrió un problema al conectarse a la base de datos", "Error en la conección", JOptionPane.CLOSED_OPTION, JOptionPane.WARNING_MESSAGE);
-        }
-    }
-
-    public void Close() {
-        try {
-            con.close();
-        } catch (SQLException e) {
         }
     }
 }
